b914ec73e50f4bdabbb92c04aa4ebca7fa2d69a8
David Dias <daviddias.p@gmail.com>
2017-03-30 08:17:12 +0100

    feat: connectedMultiaddr

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@
 const Id = require('peer-id')
 const multiaddr = require('multiaddr')
 const uniqBy = require('lodash.uniqby')
-
-exports = module.exports = PeerInfo
+const assert = require('assert')
 
 function ensureMultiaddr (addr) {
   if (multiaddr.isMultiaddr(addr)) {
@@ -15,85 +14,85 @@ function ensureMultiaddr (addr) {
 }
 
 // Peer represents a peer on the IPFS network
-function PeerInfo (peerId) {
-  if (!(this instanceof PeerInfo)) {
-    return new PeerInfo(peerId)
-  }
+class PeerInfo {
+  constructor (peerId) {
+    assert(peerId, 'Missing peerId. Use Peer.create(cb) to create one')
 
-  if (!peerId) {
-    throw new Error('Missing peerId. Use Peer.create(cb) to create one')
-  }
+    this.id = peerId
+    this.multiaddrs = []
+    this._observedMultiaddrs = []
+    this._connectedMultiaddr = undefined
 
-  this.id = peerId
+    this.multiaddr = {}
 
-  this.multiaddrs = []
-  const observedMultiaddrs = []
+    this.multiaddr.add = (addr) => {
+      addr = ensureMultiaddr(addr)
 
-  this.multiaddr = {}
-
-  this.multiaddr.add = (addr) => {
-    addr = ensureMultiaddr(addr)
-
-    var exists = false
-    this.multiaddrs.some((m, i) => {
-      if (m.equals(addr)) {
-        exists = true
-        return true
+      var exists = false
+      this.multiaddrs.some((m, i) => {
+        if (m.equals(addr)) {
+          exists = true
+          return true
+        }
+      })
+      if (!exists) {
+        this.multiaddrs.push(addr)
       }
-    })
-    if (!exists) {
-      this.multiaddrs.push(addr)
     }
-  }
 
-  // to prevent multiaddr explosion
-  this.multiaddr.addSafe = (addr) => {
-    addr = ensureMultiaddr(addr)
+    // to prevent multiaddr explosion due to Identify
+    this.multiaddr.addSafe = (addr) => {
+      addr = ensureMultiaddr(addr)
 
-    var check = false
-    observedMultiaddrs.some((m, i) => {
-      if (m.equals(addr)) {
-        this.multiaddr.add(addr)
-        observedMultiaddrs.splice(i, 1)
-        check = true
+      let check = false
+      this._observedMultiaddrs.some((m, i) => {
+        if (m.equals(addr)) {
+          this.multiaddr.add(addr)
+          this._observedMultiaddrs.splice(i, 1)
+          check = true
+        }
+      })
+      if (!check) {
+        this._observedMultiaddrs.push(addr)
       }
-    })
-    if (!check) {
-      observedMultiaddrs.push(addr)
     }
-  }
 
-  this.multiaddr.rm = (addr) => {
-    addr = ensureMultiaddr(addr)
+    this.multiaddr.rm = (addr) => {
+      addr = ensureMultiaddr(addr)
 
-    this.multiaddrs.some((m, i) => {
-      if (m.equals(addr)) {
-        this.multiaddrs.splice(i, 1)
-        return true
+      this.multiaddrs.some((m, i) => {
+        if (m.equals(addr)) {
+          this.multiaddrs.splice(i, 1)
+          return true
+        }
+      })
+    }
+
+    this.multiaddr.replace = (existing, fresh) => {
+      if (!Array.isArray(existing)) {
+        existing = [existing]
       }
-    })
+      if (!Array.isArray(fresh)) {
+        fresh = [fresh]
+      }
+      existing.forEach((m) => this.multiaddr.rm(m))
+      fresh.forEach((m) => this.multiaddr.add(m))
+    }
   }
 
-  this.multiaddr.replace = (existing, fresh) => {
-    if (!Array.isArray(existing)) {
-      existing = [existing]
-    }
-    if (!Array.isArray(fresh)) {
-      fresh = [fresh]
-    }
-    existing.forEach((m) => {
-      this.multiaddr.rm(m)
-    })
-    fresh.forEach((m) => {
-      this.multiaddr.add(m)
-    })
-  }
-
-  this.distinctMultiaddr = () => {
-    var result = uniqBy(this.multiaddrs, function (item) {
+  distinctMultiaddr () {
+    return uniqBy(this.multiaddrs, (item) => {
       return [item.toOptions().port, item.toOptions().transport].join()
     })
-    return result
+  }
+
+  setConnectedMultiaddr (ma) {
+    ma = ensureMultiaddr(ma)
+    this._connectedMultiaddr = ma
+  }
+
+  isConnected () {
+    return Boolean(this._connectedMultiaddr)
   }
 }
 
@@ -120,3 +119,5 @@ PeerInfo.isPeerInfo = (peerInfo) => {
     peerInfo.id &&
     peerInfo.multiaddrs)
 }
+
+module.exports = PeerInfo

7bd5c984abe0ed08505a7475a1922564905b9638
David Dias <daviddias.p@gmail.com>
2017-03-30 09:29:00 +0100

    new api proposal

--- src/index.js
+++ src/index.js
@@ -20,75 +20,79 @@ class PeerInfo {
 
     this.id = peerId
     this.multiaddrs = []
+    this.protocols = []
     this._observedMultiaddrs = []
     this._connectedMultiaddr = undefined
+  }
 
-    this.multiaddr = {}
+  multiaddrAdd (addr) {
+    addr = ensureMultiaddr(addr)
 
-    this.multiaddr.add = (addr) => {
-      addr = ensureMultiaddr(addr)
-
-      var exists = false
-      this.multiaddrs.some((m, i) => {
-        if (m.equals(addr)) {
-          exists = true
-          return true
-        }
-      })
-      if (!exists) {
-        this.multiaddrs.push(addr)
+    var exists = false
+    this.multiaddrs.some((m, i) => {
+      if (m.equals(addr)) {
+        exists = true
+        return true
       }
-    }
-
-    // to prevent multiaddr explosion due to Identify
-    this.multiaddr.addSafe = (addr) => {
-      addr = ensureMultiaddr(addr)
-
-      let check = false
-      this._observedMultiaddrs.some((m, i) => {
-        if (m.equals(addr)) {
-          this.multiaddr.add(addr)
-          this._observedMultiaddrs.splice(i, 1)
-          check = true
-        }
-      })
-      if (!check) {
-        this._observedMultiaddrs.push(addr)
-      }
-    }
-
-    this.multiaddr.rm = (addr) => {
-      addr = ensureMultiaddr(addr)
-
-      this.multiaddrs.some((m, i) => {
-        if (m.equals(addr)) {
-          this.multiaddrs.splice(i, 1)
-          return true
-        }
-      })
-    }
-
-    this.multiaddr.replace = (existing, fresh) => {
-      if (!Array.isArray(existing)) {
-        existing = [existing]
-      }
-      if (!Array.isArray(fresh)) {
-        fresh = [fresh]
-      }
-      existing.forEach((m) => this.multiaddr.rm(m))
-      fresh.forEach((m) => this.multiaddr.add(m))
+    })
+    if (!exists) {
+      this.multiaddrs.push(addr)
     }
   }
 
-  distinctMultiaddr () {
+  // to prevent multiaddr explosion due to Identify
+  multiaddrAddSafe (addr) {
+    addr = ensureMultiaddr(addr)
+
+    let check = false
+    this._observedMultiaddrs.some((m, i) => {
+      if (m.equals(addr)) {
+        this.multiaddr.add(addr)
+        this._observedMultiaddrs.splice(i, 1)
+        check = true
+      }
+    })
+    if (!check) {
+      this._observedMultiaddrs.push(addr)
+    }
+  }
+
+  multiaddrRemove (addr) {
+    addr = ensureMultiaddr(addr)
+
+    this.multiaddrs.some((m, i) => {
+      if (m.equals(addr)) {
+        this.multiaddrs.splice(i, 1)
+        return true
+      }
+    })
+  }
+
+  multiaddrReplace (existing, fresh) {
+    if (!Array.isArray(existing)) {
+      existing = [existing]
+    }
+    if (!Array.isArray(fresh)) {
+      fresh = [fresh]
+    }
+    existing.forEach((m) => this.multiaddr.rm(m))
+    fresh.forEach((m) => this.multiaddr.add(m))
+  }
+
+  multiaddrUniq () {
     return uniqBy(this.multiaddrs, (item) => {
       return [item.toOptions().port, item.toOptions().transport].join()
     })
   }
 
-  setConnectedMultiaddr (ma) {
+  // only stores the current multiaddr being used
+  connect (ma) {
     ma = ensureMultiaddr(ma)
     this._connectedMultiaddr = ma
+  }
+
+  disconnect () {
+    this._connectedMultiaddr = undefined
   }
 
   isConnected () {
